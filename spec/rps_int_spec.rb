0c2842d7260cedf4b7a0b85130e5d6cc4b74854e
Jim Patt & alex caste <student@epicodus.com>
2015-04-22 15:33:22 -0700

    final code

--- spec/rps_int_spec.rb
+++ spec/rps_int_spec.rb
@@ -7,7 +7,7 @@ describe('the rock paper scissor path', {:type => :feature}) do
     visit('/')
     fill_in('user1' , :with => 'rock')
     fill_in('user2' , :with => 'scissors')
-    click_button('Go')
+    click_button('FIGHT!')
     expect(page).to have_content('Rock Wins! Take that, Player 2!')
   end
 end
