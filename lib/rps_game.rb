83fc28feee9e4e570414f458f79a62aa04acf5f7
Jim Patt & alex caste <student@epicodus.com>
2015-04-22 14:36:32 -0700

    all spec pass for code

--- lib/rps_game.rb
+++ lib/rps_game.rb
@@ -1,27 +1,24 @@
 class String
   define_method(:beats?) do |user2|
     user1 = self
-    # user1_score = 0
-    # user2_score = 0
 
 
     if user1.eql?("rock") && user2.eql?("scissors")
-      # user1_score = user1_score.+(1)
-      "Rock Wins! You da man, Player 1!"
+      "Rock Wins! Take that, Player 2!"
     elsif user1.eql?("scissors") && user2.eql?("paper")
-      return "Scissor Wins!"
+      "Scissor Wins! Suck it, Player 2!"
     elsif user1.eql?("paper") && user2.eql?("rock")
-      return "Paper Wins!"
+      "Paper Wins! Way to suck, Player 2!"
     elsif user2.eql?("rock") && user1.eql?("scissors")
-      # user1_score = user1_score.+(1)
-      "Rock Wins! You da man, Player 2"
+      "Rock Wins! You suck, Player 1!"
     elsif user2.eql?("scissors") && user1.eql?("paper")
-      return "Scissor Wins!"
+      "Scissor Wins! In yo face Player 1"
     elsif user2.eql?("paper") && user1.eql?("rock")
-      return "Paper Wins!"
+      "Paper Wins! Eat it, Player 1!"
     else user1 == user2
-      return "It's a Tie!"
+      "It's a Tie!  Damn, you both suck."
     end
 
   end
+
 end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -5,26 +5,28 @@ require('pry')
 describe('String#beats?') do
   #make rock beat scissors
   it("returns true if rock is the object and scissors is the argument") do
-    expect("rock".beats?("scissors")).to(eq("Rock Wins! You da man, Player 1!"))
+    expect("rock".beats?("scissors")).to(eq("Rock Wins! Take that, Player 2!"))
   end
 
   #make scissors beat paper
   it("returns true if scissors is the object and paper is the argument") do
-    expect("scissors".beats?("paper")).to(eq("Scissor Wins!"))
+    expect("scissors".beats?("paper")).to(eq("Scissor Wins! Suck it, Player 2!"))
   end
 
   #make paper beat rock
   it("returns true if paper is the object and rock is the argument") do
-    expect("paper".beats?("rock")).to(eq("Paper Wins!"))
+    expect("paper".beats?("rock")).to(eq("Paper Wins! Way to suck, Player 2!"))
   end
 
   #make matching results tie
   it("returns true if rock is the object and rock is the argument") do
-    expect("rock".beats?("rock")).to(eq("It's a Tie!"))
+    expect("rock".beats?("rock")).to(eq("It's a Tie!  Damn, you both suck."))
   end
 
   #make rock beats scissor for player 2 only
   it("returns true if rock is the object and scissors is the argument") do
-    expect("rock".beats?("scissors")).to(eq("Rock Wins! You da man, Player 2!"))
+    expect("scissors".beats?("rock")).to(eq("Rock Wins! You suck, Player 1!"))
   end
+
+
 end
